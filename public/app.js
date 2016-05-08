8b5f6a9dc47d9c51a6f71c917a121776a5ab3e01
Andrew Smith <andrew.smith@maxwellhealth.com>
2016-05-08 00:55:17 -0400

    move profile panel to function

--- public/app.js
+++ public/app.js
@@ -13,73 +13,76 @@ $(document).ready(function() {
       return false;
     }
     $.get('api/summoner/' + summonername + '/' + region, function(summonerData) {
-      var champions = summonerData.champions;
-      var roles = {
-        Assassin: 0,
-        Fighter: 0,
-        Mage: 0,
-        Marksman: 0,
-        Support: 0,
-        Tank: 0
-      };
-
-      $('#prevplayed').empty();
-
-      $.each(champions, function(index, champion) {
-        var tags = champion.tags;
-
-        if (tags.length === 1) {
-          tags[1] = tags[0];
-        }
-
-        roles[tags[0]] += 2;
-        roles[tags[1]] += 1;
-
-        if (index < 6) {
-          var championIcon = $('<img />');
-          championIcon.attr('src', 'http://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/' + champion.key + '.png');
-          championIcon.appendTo('#prevplayed');
-        }
-
-      });
-      
-      $('#profile').show();
-      currentChart && currentChart.destroy();
-
-      var ctx = $('#chart');
-      ctx.width('100px');
-      ctx.height('100px');
-      currentChart = new Chart(ctx, {
-        type: 'doughnut',
-        cutoutPercentage: 0,
-        data: {
-          labels: ["Assassin", "Fighter", "Mage", "Marksman", "Support", "Tank"],
-          datasets: [{
-            label: '# of Votes',
-            data: [
-              roles['Assassin'],
-              roles['Fighter'],
-              roles['Mage'], 
-              roles['Marksman'],
-              roles['Support'],
-              roles['Tank']
-            ],
-            backgroundColor: [
-              "#FFC994",
-              "#BC486F",
-              "#2B818C",
-              "#7123AF",
-              "#990069",
-              "#FF9877"
-            ],
-            borderWidth: [0,0,0,0,0,0]
-          }]
-        }
-      });
+      createProfilePanel(summonerData.champions);
       fetchRecommendations(summonerData.id, region);
       
     });
   });
+
+  function createProfilePanel(champions) {
+    $('#prevplayed').empty();
+
+    var roles = {
+      Assassin: 0,
+      Fighter: 0,
+      Mage: 0,
+      Marksman: 0,
+      Support: 0,
+      Tank: 0
+    };
+
+    $.each(champions, function(index, champion) {
+      var tags = champion.tags;
+
+      if (tags.length === 1) {
+        tags[1] = tags[0];
+      }
+
+      roles[tags[0]] += 2;
+      roles[tags[1]] += 1;
+
+      if (index < 6) {
+        var championIcon = $('<img />');
+        championIcon.attr('src', 'http://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/' + champion.key + '.png');
+        championIcon.appendTo('#prevplayed');
+      }
+
+    });
+    
+    $('#profile').show();
+    currentChart && currentChart.destroy();
+
+    var ctx = $('#chart');
+    ctx.width('100px');
+    ctx.height('100px');
+    currentChart = new Chart(ctx, {
+      type: 'doughnut',
+      cutoutPercentage: 0,
+      data: {
+        labels: ["Assassin", "Fighter", "Mage", "Marksman", "Support", "Tank"],
+        datasets: [{
+          label: '# of Votes',
+          data: [
+            roles['Assassin'],
+            roles['Fighter'],
+            roles['Mage'], 
+            roles['Marksman'],
+            roles['Support'],
+            roles['Tank']
+          ],
+          backgroundColor: [
+            "#FFC994",
+            "#BC486F",
+            "#2B818C",
+            "#7123AF",
+            "#990069",
+            "#FF9877"
+          ],
+          borderWidth: [0,0,0,0,0,0]
+        }]
+      }
+    });
+  }
 
   function fetchRecommendations(summonerId, region) {
     $.get('api/recommendations/' + summonerId + '/' + region, function(recommendations) {
